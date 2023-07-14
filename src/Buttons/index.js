4df248ff743272a18aeaccc47eb2faddc48e988a
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-07-14 16:47:12 +0200

    hideDoneTasks rename to hideDone

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,11 +1,11 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDoneTasks }) => (
+const Buttons = ({ tasks, hideDone }) => (
   <div className="container">
     {tasks.length > 0 && (
       <>
         <button className="container__button">
-          {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+          {hideDone ? "Pokaż" : "Ukryj"} ukończone
         </button>
         <button
           className="container__button"
