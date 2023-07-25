ba71c178f9868d5d7e43ce18a76b8a6767260e6c
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-07-25 22:45:07 +0200

    adding brackets to import useTasks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import useTasks from "./useTasks";
+import { useTasks } from "./useTasks";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Header from "./Header";
