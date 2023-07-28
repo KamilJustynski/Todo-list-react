a4255effbc388952d7258b9c9e9701457c6e77ba
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-07-28 17:20:53 +0200

    adding globalStyle

---
+++ src/globalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+    }
+
+    *,
+    ::after,
+    ::before {
+        box-sizing: inherit;
+    }
+
+    body {
+        background-image: url(https://wallpapers.com/images/high/best-marble-background-6v2qysqi0lvrsu40.webp);
+        opacity: 0.9;
+        background-size: cover;
+        font-family: "Lato", sans-serif;
+        margin: 20px;
+    }`;
