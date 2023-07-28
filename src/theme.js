354dc4547d73e23c618ff5ff9656b80fd709c638
Kamil Justyński <kamil.justynski1998@gmail.com>
2023-07-28 18:41:35 +0200

    changing color notation

--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,14 @@
 export const theme = {
   color: {
-    white: "white",
-    teal: "teal",
+    white: "#ffffff",
+    teal: "#008080",
     gray: "#ccc",
     mediumGray: "#ddd",
     lightTeal: "hsl(180, 100%, 35%)",
     mediumTeal: "hsl(180, 100%, 30%)",
-    green: "green",
+    green: "#008000",
     mediumGreen: "hsl(120, 100%, 30%)",
-    red: "red",
+    red: "#ff0000",
     mediumRed: "hsl(0, 100%, 65%)",
   },
 
